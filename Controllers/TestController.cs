a26f3ba7be3ea55d8e4c4c35d8679686d3a5b639
Dravvos <daniddias53@gmail.com>
2023-10-18 19:23:17 -0300

    feat: add test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIClient.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
